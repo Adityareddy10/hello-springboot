b6eb1c8722e801cd796ba288e7c5ff96a1b7a133
EternalOne1 <tom.jerry103011@gmail.com>
2026-01-13 19:08:41 +0530

    Connect Spring Boot to Railway MySQL

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -1,14 +1,28 @@
 package com.example;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 public class HelloController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/")
     public String hello() {
         return "Hello World";
+    }
+
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping("/users")
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
     }
 
 }
